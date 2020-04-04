23ad75c695c73ff8bf79cc3d7774a2afb82983e1
Matt Dambra <mpdambra@gmail.com>
2020-04-04 18:34:01 -0400

    adding listeners to servicework

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,2 +1,49 @@
 const FILE_CACHE = "static-cache-v1",
- DATA_CACHE_NAME = "data-cache-v1"
+    DATA_CACHE = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.js',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+self.addEventListener("install", function(e){
+    e.waitUntil(
+        caches.open(FILE_CACHE).then(function(cache){
+            console.log("Cache opened");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    )
+});
+
+self.addEventListener("fetch", function(e){
+    if(event.request.url.includes("/api")){
+        e.respondWith(
+            caches.open(DATA_CACHE).then(cache => {
+                return fetch(e.request)
+                    .then(response => {
+                        if(response.status === 200) {
+                            cache.put(event.request.url, response.clone())
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        cache.match(event.request.url);
+                    })
+            })
+        )
+        return;
+    }
+
+    e.respondeWith(fetch(e.request).catch(function() {
+        return caches.match(e.request).then(res => {
+            if(res){
+                return res;
+            } else{
+                return "/";
+            }
+        })
+    }))
+});
